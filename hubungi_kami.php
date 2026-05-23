76b6266a9a4f05384236b876a1730648f4050183
cahaya1112 <cahayawulandari1112@gmail.com>
2026-05-23 14:26:50 +0700

    Update hubungi_kami.php

--- hubungi_kami.php
+++ hubungi_kami.php
@@ -346,7 +346,7 @@ if(isset($_POST['kirim'])){
         <div class="info-icon"><i class="fa-solid fa-location-dot"></i></div>
         <h3>Kantor</h3>
         <p> Ds.benelan kidul <br>Banyuwangi, Jawa Timur</p>
-        <a href="#">Lihat di Maps</a>
+        <!-- <a href="#">Lihat di Maps</a> -->
     </div>
 </section>
 
